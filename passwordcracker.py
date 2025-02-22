6a84f0e2305e281fef4545b6245c462bf969f1e0
asif minar <asifminar7@gmail.com>
2025-02-23 01:44:44 +0600

    step1

--- passwordcracker.py
+++ passwordcracker.py
@@ -1,1 +1,14 @@
-print("test")
+#step 1
+
+import hashlib
+
+def make_md5_hash(password):
+    hash_object = hashlib.md5(password.encode())
+    return hash_object.hexdigest()
+
+test_password = "password"
+hashed = make_md5_hash(test_password)
+print("password is: ", test_password)
+print("MD5 hash of password is: ", hashed)
+    
+    
